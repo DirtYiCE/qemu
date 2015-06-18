9091fb3e2046f4d23c8209d89883a902c5079332
Kővágó, Zoltán <DirtY.iCE.hu@gmail.com>
2015-06-18 18:24:18 +0200

    audio: -audiodev command line option
    This patch adds an -audiodev command line option, and deprecates the
    QEMU_* environment variables for audio backend configuration.  It's
    syntax is similar to existing options (-netdev, -device, etc): -audiodev
    driver_name,property=value,...
    
    Audio drivers now get an Audiodev * as config paramters, instead of the
    global audio_option structs.  There is some code in audio/audio_legacy.c
    that converts the old environment variables to audiodev options (this
    way backends do not have to worry about legacy options).  It also
    contains a replacement of -audio-help, which prints out the equivalent
    -audiodev based config of the currently specified environment variables.
    
    Although now it's possible to specify multiple -audiodev options on
    command line, multiple audio backends are not supported yet.
    
    Signed-off-by: Kővágó, Zoltán <DirtY.iCE.hu@gmail.com>
    
    ---
    
    Changes from v2:
    * fixed alsa threshold
    * updated to audio qapi changes
    
    Changes from v1:
    * updated to everything usecs without suffix
    * better rounding when converting between usecs and frames/samples/bytes
    * bugfixes in audio_legacy.c
    * fixed code broken by qemu_opts changes by Markus

--- audio/noaudio.c
+++ audio/noaudio.c
@@ -134,7 +134,7 @@ static int no_ctl_in (HWVoiceIn *hw, int cmd, ...)
     return 0;
 }
 
-static void *no_audio_init (void)
+static void *no_audio_init (Audiodev *dev)
 {
     return &no_audio_init;
 }
@@ -161,7 +161,6 @@ static struct audio_pcm_ops no_pcm_ops = {
 struct audio_driver no_audio_driver = {
     .name           = "none",
     .descr          = "Timer based audio emulation",
-    .options        = NULL,
     .init           = no_audio_init,
     .fini           = no_audio_fini,
     .pcm_ops        = &no_pcm_ops,
